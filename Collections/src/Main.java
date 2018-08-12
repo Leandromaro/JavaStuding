ccc518e4e02cc9128862b872593f27a456ac4deb
Leandro Maro <leandro.maro@globant.com>
2018-08-12 18:29:40 -0300

    Adding maps examples

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -1,9 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -11,9 +6,9 @@ public class Main {
     public static Collection<Person> people = new ArrayList<>();
 
     public static void  main(String[]args){
-        Person personOne = new Main().new Person("Leandro", "Maro", Gender.male);
-        Person personTwo = new Main().new Person("Leandro", "Maro", Gender.male);
-        Person personThree = new Main().new Person("Eugenia", "Nuñez", Gender.female);
+        Person personOne = new Main().new Person("Leandro", "Maro", Gender.MALE, 33, City.RESISTENCIA);
+        Person personTwo = new Main().new Person("Leandro", "Maro", Gender.MALE, 33, City.RESISTENCIA);
+        Person personThree = new Main().new Person("Eugenia", "Nuñez", Gender.FEMALE, 30, City.CORRIENTES);
 
         people.add(personOne);
         people.add(personTwo);
@@ -41,6 +36,19 @@ public class Main {
                 .map(Person::getName)
                 .collect(Collectors.toCollection(LinkedHashSet::new));
         LinkedHashSet.stream().forEach(System.out::println);
+
+        // Maps
+        // Collecting maps from a List and grouping them into maps
+        System.out.printf("\n");
+        System.out.printf("Maps\n");
+        System.out.printf("\n");
+        Map<Integer, List<Person>> peopleMap = people.stream().collect(Collectors.groupingBy(Person::getAge));
+        Map<Boolean, List<Person>> peopleMapByAge = people.stream().collect(Collectors.partitioningBy(p -> p.age > 30));
+        Map<City, Integer> peopleByCitySumAge = people.stream().collect(Collectors.groupingBy(Person::getCity, Collectors.summingInt(p -> p.age)));
+
+        System.out.println(peopleMap.entrySet() + "\n");
+        System.out.println(peopleMapByAge.values()+ "\n");
+        System.out.println(peopleByCitySumAge.values()+ "\n");
     }
 
     class Person{
@@ -48,39 +56,74 @@ public class Main {
         private String name;
         private String lastName;
         private Gender gender;
+        private int age;
+        private City city;
 
-        public Person(String name, String lastName, Gender gender) {
+        public Person(String name, String lastName, Gender gender, int age, City city) {
             this.name = name;
             this.lastName = lastName;
             this.gender = gender;
+            this.age = age;
+            this.city = city;
         }
 
         public String getName() {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
         public String getLastName() {
             return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
         }
 
         public Gender getGender() {
             return gender;
         }
 
+        public int getAge() {
+            return age;
+        }
+
+        public City getCity() {
+            return city;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
         public void setGender(Gender gender) {
             this.gender = gender;
         }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setCity(City city) {
+            this.city = city;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", gender=" + gender +
+                    ", age=" + age +
+                    ", city=" + city +
+                    '}';
+        }
+    }
+
+    enum City{
+        RESISTENCIA, CORRIENTES;
     }
 
     enum Gender {
-        male, female;
+        MALE, FEMALE;
     }
 }
