557060f6e3e260cec82fdba97bdcb84114ecd6e0
Leandro Maro <leandro.maro@globant.com>
2018-08-18 20:16:53 -0300

    Adding another functional interface

---
+++ FunctionalInterfaces/src/FunctionalInterface.java
@@ -0,0 +1,3 @@
+public interface FunctionalInterface {
+    double average();
+}
---
+++ FunctionalInterfaces/src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String args[]){
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        FunctionalInterface functionalInterface = () ->{
+            OptionalDouble average = eval(list, n -> n % 2 == 0).stream().mapToInt(Integer::intValue).average();
+            return average.getAsDouble();
+        };
+        System.out.println(functionalInterface.average());
+    }
+
+    public static List<Integer> eval(List<Integer> list, Predicate<Integer> predicate) {
+        return list.stream().filter(integer ->  predicate.test(integer)).collect(Collectors.toList());
+    }
+}
