4c45ff7beb7cffd59c80696b90d0671fe8005dd0
Leandro Maro <leandro.maro@globant.com>
2018-08-19 19:46:40 -0300

    Adding annotation

--- FunctionalInterfaces/src/Main.java
+++ FunctionalInterfaces/src/Main.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String args[]){
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        FunctionalInterface functionalInterface = () ->{
+        MathDoubleOperator functionalInterface = () ->{
             OptionalDouble average = eval(list, n -> n % 2 == 0).stream().mapToInt(Integer::intValue).average();
             return average.getAsDouble();
         };
